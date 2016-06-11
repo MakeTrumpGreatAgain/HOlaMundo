69ec0e3a66bbebcb922ac870b45d31e741d7a2c6
ManuelBlanc <manuel.blanc@estudiante.uam.es>
2016-06-11 14:06:53 +0200

    Añadido .gitignore e inicializador de venv

---
+++ init.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+if [[ ! $- == *i* ]]; then
+	>&2 echo "Must be running an interactive terminal"
+	>&2 echo "Please use ``source $0'' instead"
+	exit 1
+
+fi
+
+
+# Safe subshell
+(
+set -ue
+
+# Go to the directory containing the script
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Create the venv if it doesn't exist
+if [[ ! -d "venv" ]]; then
+	echo "Creating virtual env in venv/ ..."
+	virtualenv venv
+
+	echo "Installing python-telegram-bot ..."
+	pip install python-telegram-bot
+fi
+
+echo "Sourcing venv/bin/activate ..."
+source venv/bin/activate
+
+echo "Done!"
+
+)
+# End of safe subshell
+
+
